fe8c52662ebd73e7fe1d455d36fc39835c2ccd8f
Lina Alami <linaalamichentoufi@gmail.com>
2025-11-20 14:31:20 +0100

    Initial commit SmartMove microservices

---
+++ meteo-service/src/test/java/org/example/meteoservice/MeteoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meteoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ traffic-service/src/test/java/org/example/trafficservice/TrafficServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trafficservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
